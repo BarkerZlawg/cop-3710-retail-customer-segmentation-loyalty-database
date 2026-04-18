9160254f1c2abf04f8d535d14db2171a32157271
BarkerZlawg <kmendez6094@floridapoly.edu>
2026-04-17 21:55:01 -0400

    Initialize Streamlit app with database connection
    This initial commit sets up a Streamlit application that connects to an Oracle database. It includes multiple features for viewing customers, orders, products, and more, with appropriate UI elements for user interaction.

---
+++ app.py
@@ -0,0 +1,492 @@
+import oracledb as odb
+import streamlit as st
+import pandas as pd
+#Before we start, I wanted to clarify that due to some technical issues I am working with my own database for
+#this project (As instructed by Professor Selim). I am aware that there is only 80% credit for working solo,
+#but I still wanted to go the extra mile and do double the work to see if I could get the 100% credit even
+#though I am working alone.
+
+#To start off, we must establish a connection to our database.
+connect = odb.connect(user = 'retail_user', password = 'password', dsn = 'localhost:1521/XEPDB1')#
+cursor = connect.cursor()
+#This also initializes our "home" UI page, which will be important for the program.
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+
+#This function will allow us to go through the several different sections in our app.
+def go_to(page_name):
+    st.session_state.page = page_name
+
+#This function acts as a basis for my app to display desired information to the user.
+def run_query(query, params = None):
+    cursor.execute(query, params or {})
+    columns = [col[0] for col in cursor.description]
+
+    rows = cursor.fetchall()
+    if not rows:
+        st.warning("No results found.")
+        return
+    
+    df = pd.DataFrame(rows, columns = columns)
+    st.dataframe(df, use_container_width = True)
+
+#From this point, I will write different feature functions for our app. Our functions will likely start with 
+#user-given parameters. Then, we will use a query to pull information that fits the user's request, and display
+#it to the user.
+
+#For the first feature, we will simply view all of the customers in our database.
+def view_all_cus_screen():
+    st.title("All Customers On Record")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Generate Customer Results"):
+            query = """
+            SELECT customer_id, customer_fname, customer_lname, customer_email
+            FROM customer
+            ORDER BY customer_fname, customer_lname
+            """
+            params = {}
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the second feature, the function will check for user orders within a given date range.
+def orders_within_date_screen():
+    st.title("Orders Within a Given Date Range")
+    start = st.text_input("Enter Your Starting Date (YYYY-MM-DD): ", key = "start_date")
+    end = st.text_input("Enter Your Ending Date (YYYY-MM-DD): ", key = "end_date")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Search Orders"):
+            if not start.strip() or not end.strip():
+                st.error("Error: One or more of your fields is missing.")
+                return
+            
+            if start > end:
+                st.error("Error: Your start date cannot be larger than your end date.")
+                return
+
+            query = """
+            SELECT order_id, order_date, customer_id
+            FROM orders
+            WHERE (order_date BETWEEN TO_DATE(:start_date, 'YYYY-MM-DD') AND TO_DATE(:end_date, 'YYYY-MM-DD'))
+            """
+
+            params = {
+                "start_date": start,
+                "end_date": end
+            }
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the third feature, the function will check for a customer's purchase history.
+def customer_purchase_history_screen():
+    st.title("Customer Purchase History")
+    cus_id = st.text_input("Enter your customer's ID: ", key = "desired_cus_id")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Search Purchase History"):
+            if not cus_id.strip():
+                st.error("Error: Your field is missing.")
+                return
+            
+            if not cus_id.isdigit():
+                st.error("Error: Your ID must be a number.")
+                return
+
+            query = """
+            SELECT c.customer_fname, c.customer_lname, o.order_id, p.product_name, op.quantity
+            FROM customer c
+            JOIN orders o ON c.customer_id = o.customer_id
+            JOIN order_product op ON o.order_id = op.order_id
+            JOIN product p ON op.product_id = p.product_id
+            WHERE c.customer_id = :desired_cus_id
+            """
+            params = {
+            "desired_cus_id": int(cus_id)
+            }
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the fourth feature, the function will view all products that match a given category.
+def products_in_category_screen():
+    st.title("Products Within a Given Category")
+    category = st.text_input("Enter the Product Category: ", key = "desired_cat")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Search Category"):
+            if not category:
+                st.error("Error: Your field is missing.")
+                return
+            
+            query = """
+            SELECT p.product_name, p.product_price, c.category_name
+            FROM product p
+            JOIN category c ON p.category_id = c.category_id
+            WHERE c.category_name = :desired_cat
+            """
+            params = {
+                "desired_cat": category
+            }
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")       
+
+
+#For the fifth feature, the function will check for the top 10 selling products.
+def top_selling_products_screen():
+    st.title("Top Selling Products on Record")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Generate Top 10 Product Results"):
+            query = """
+            SELECT p.product_name, SUM(op.quantity) AS total_sold
+            FROM order_product op
+            JOIN product p ON op.product_id = p.product_id
+            GROUP BY p.product_id, p.product_name
+            ORDER BY total_sold DESC, p.product_name ASC
+            FETCH FIRST 10 ROWS ONLY
+            """
+            params = {}
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the sixth feature, the function will find and retrieve all order details of a given order ID.
+def specific_order_dets_screen():
+    st.title("View Specific Order Details")
+    des_order_id = st.text_input("Provide Your Desired Order ID: ", key = "desired_order")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Search Order Details"):
+            if not des_order_id.strip():
+                st.error("Error: Your field is missing.")
+                return
+            
+            if not des_order_id.isdigit():
+                st.error("Error: Your ID must be a number.")
+                return
+
+            query = """
+            SELECT o.order_id, p.product_name, op.quantity, p.product_price,
+                (op.quantity * p.product_price) AS total_price
+            FROM orders o
+            JOIN order_product op ON o.order_id = op.order_id
+            JOIN product p ON op.product_id = p.product_id
+            WHERE o.order_id = :desired_order
+            """
+            params = {
+                "desired_order": int(des_order_id)
+            }
+
+            run_query(query, params)
+        
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the seventh feature, the function will check the total amount spent by a given customer, and their
+#ranking on "Most Spent" compared to other customers.
+def cus_spend_amt_screen():
+    st.title("View a Customer's Total Spent")
+    cus_id = st.text_input("Enter your customer's ID: ", key = "desired_cus_id")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Check Customer's Total Spent"):
+            if not cus_id.strip():
+                st.error("Error: your field is missing.")
+                return
+            
+            if not cus_id.isdigit():
+                st.error("Error: Your ID must be a number.")
+                return
+
+            query = """
+            SELECT *
+            FROM (
+                SELECT c.customer_id, c.customer_fname, c.customer_lname,
+                    SUM(op.quantity * p.product_price) AS total_spent,
+                    RANK() OVER (ORDER BY SUM(op.quantity * p.product_price) DESC) as spend_rank
+                FROM customer c
+                JOIN orders o ON c.customer_id = o.customer_id
+                JOIN order_product op ON o.order_id = op.order_id
+                JOIN product p ON op.product_id = p.product_id
+                GROUP BY c.customer_id, c.customer_fname, c.customer_lname
+            )
+            WHERE customer_id = :desired_cus_id
+            """
+            params = {
+                "desired_cus_id": int(cus_id)
+            }
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the eighth feature, the function will check for the top 10 highest spending customers on the database.
+def top_spending_cus_screen():
+    st.title("Top 10 All-Time Highest Spenders")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Generate Top 10 Spender Results"):
+            query = """
+            SELECT c.customer_id, c.customer_fname, c.customer_lname,
+                SUM(op.quantity * p.product_price) as total_spent
+            FROM customer c
+            JOIN orders o ON c.customer_id = o.customer_id
+            JOIN order_product op  ON o.order_id = op.order_id
+            JOIN product p ON op.product_id = p.product_id
+            GROUP BY c.customer_id, c.customer_fname, c.customer_lname
+            ORDER BY total_spent DESC
+            FETCH FIRST 10 ROWS ONLY
+            """
+            params = {}
+
+            run_query(query, params)
+    
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the ninth feature, the function will pull the top 25 selling products from a given time range.
+def top_products_date_range_screen():
+    st.title("Top 25 Products Over a Given Date Range")
+    start = st.text_input("Enter Your Starting Date (YYYY-MM-DD): ", key = "top_start_date")
+    end = st.text_input("Enter Your Ending Date (YYYY-MM-DD): ", key = "top_end_date")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Generate Top 25 Products"):
+            if not start.strip() or not end.strip():
+                st.error("Error: One or more of your fields is missing.")
+                return
+            
+            if start > end:
+                st.error("Error: Your start date cannot be larger than your end date.")
+                return
+
+            query = """
+            SELECT p.product_name, SUM(op.quantity) AS total_sold
+            FROM orders o
+            JOIN order_product op ON o.order_id = op.order_id
+            JOIN product p ON op.product_id = p.product_id
+            WHERE (o.order_date BETWEEN TO_DATE(:start_date, 'YYYY-MM-DD') AND TO_DATE(:end_date, 'YYYY-MM-DD'))
+            GROUP BY p.product_id, p.product_name
+            ORDER BY total_sold DESC
+            FETCH FIRST 25 ROWS ONLY
+            """
+
+            params = {
+                "start_date": start,
+                "end_date": end
+            }
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the tenth feature, this function will find any customers with no recorded orders.
+def customers_no_orders_screen():
+    st.title("Customers With No Current Orders")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Find Customers"):
+            query = """
+            SELECT c.customer_id, c.customer_fname, c.customer_lname
+            FROM customer c
+            LEFT JOIN orders o ON c.customer_id = o.customer_id
+            WHERE o.order_id IS NULL
+            """
+            params = {}
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the eleventh feature, this function will pull the categories that have sold the most overall.
+def most_popular_category_screen():
+    st.title("Most Popular Product Categories")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("Generate Category Rankings"):
+            query = """
+            SELECT c.category_name, SUM(op.quantity) AS total_sold
+            FROM category c
+            JOIN product p ON c.category_id = p.category_id
+            JOIN order_product op ON p.product_id = op.product_id
+            GROUP BY c.category_name
+            ORDER BY total_sold DESC
+            """
+            params = {}
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+
+#For the twelth and final category (sorry for putting you through this grader), the function will prompt the
+#user to input a product name, and it will pull said product and its attributes.
+def search_product_screen():
+    st.title("Search For a Product")
+    product_name = st.text_input("Enter Your Desired Product (Or a Portion of a Product Name): ", key = "search_product")
+    col1, col2 = st.columns(2)
+
+    with col1: 
+        if st.button("Search For Your Product: "):
+            if not product_name.strip():
+                st.error("Error: Your field is missing.")
+                return
+            
+            if len(product_name) > 50:
+                st.error("Error: Your input is greater than the max possible length.")
+                return
+            
+            query = """
+            SELECT p.product_id, p.product_name, p.product_price,
+                NVL(SUM(op.quantity), 0) AS total_sold
+            FROM product p
+            LEFT JOIN order_product op ON p.product_id = op.product_id
+            WHERE LOWER(p.product_name) LIKE LOWER('%' || :prod_name || '%')
+            GROUP BY p.product_id, p.product_name, p.product_price
+            ORDER BY total_sold DESC
+            """
+            params = {
+                "prod_name": product_name
+            }
+
+            run_query(query, params)
+
+    with col2:
+        if st.button("Back to Home"):
+            go_to("home")
+
+#After defining all of the functions, we will make a home screen with all of the required buttons to reach
+#each function.
+def home_screen():
+    st.title("Retail CRM Dashboard")
+    st.write("Select an Option:")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        if st.button("View All Customers"):
+            go_to("view_all_cus")
+
+        if st.button("View Orders Within a Given Date Range"):
+            go_to("orders_within_date")
+
+        if st.button("View a Customer's Purchase History"):
+            go_to("customer_purchase_history")
+
+        if st.button("View Products of a Given Category"):
+            go_to("products_in_category")
+
+    with col2:
+        if st.button("View Top 10 Selling Products"):
+            go_to("top_selling_products")
+
+        if st.button("Search Specific Order Details"):
+            go_to("specific_order_dets")
+
+        if st.button("View a Customer's Total Spent"):
+            go_to("cus_spend_amt")
+
+        if st.button("View Top 10 Spending Customers"):
+            go_to("top_spending_cus")
+
+    with col3:
+        if st.button("View Top Selling Product Within A Given Date Range"):
+            go_to("top_products_date_range")
+
+        if st.button("View Customers With No Recorded Orders"):
+            go_to("customers_no_orders")
+
+        if st.button("View Most Popular Categories"):
+            go_to("most_popular_category")
+
+        if st.button("Search For a Product By Name"):
+            go_to("search_product")
+
+
+#These final if-statements check for the current session page based on given inputs.
+if st.session_state.page == "home":
+    home_screen()
+
+elif st.session_state.page == "view_all_cus":
+    view_all_cus_screen()
+
+elif st.session_state.page == "orders_within_date":
+    orders_within_date_screen()
+
+elif st.session_state.page == "customer_purchase_history":
+    customer_purchase_history_screen()
+
+elif st.session_state.page == "products_in_category":
+    products_in_category_screen()
+
+elif st.session_state.page == "top_selling_products":
+    top_selling_products_screen()
+
+elif st.session_state.page == "specific_order_dets":
+    specific_order_dets_screen()
+
+elif st.session_state.page == "cus_spend_amt":
+    cus_spend_amt_screen()
+
+elif st.session_state.page == "top_spending_cus":
+    top_spending_cus_screen()
+
+elif st.session_state.page == "top_products_date_range":
+    top_products_date_range_screen()
+
+elif st.session_state.page == "customers_no_orders":
+    customers_no_orders_screen()
+
+elif st.session_state.page == "most_popular_category":
+    most_popular_category_screen()
+
+elif st.session_state.page == "search_product":
+    search_product_screen()
